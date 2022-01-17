36f77cc7c87f83d55075f3373182694b6e962045
RobertoCubensis <roberto@cubensisproject.com>
2022-01-17 17:56:10 +0100

    Creación de rama alternativa, y modificación del Index

--- gitTest/index.php
+++ gitTest/index.php
@@ -4,13 +4,7 @@
 	<title>Git Test</title>
 </head>
 <body>
-<h2>Git Test 01</h2>
-<i>testing</i>
-<h2>Git Test 02</h2>
-<i>testing</i>
-<h2>Git Test 03</h2>
-<i>testing</i>
-<h2>Git Test 04</h2>
+<h2>Git Test 11 - Rama alternativa</h2>
 <i>testing</i>
 
 </body>
